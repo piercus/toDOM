0888899e4ea1a609f9464ff7daca791ac64fe869
pierre <piercus@gmail.com>
2013-01-17 01:44:08 +0100

    doc changes

--- README.md
+++ README.md
@@ -11,6 +11,8 @@
 Here is toDOM :
 ``` javascript
 
+var toDOM = sand.require("toDOM/toDOM");
+
 var scope = {};
 
 var el = toDOM({
@@ -20,16 +22,14 @@ var el = toDOM({
 			console.log('div clicked');
 	  },
   },
-  children : [
-  	{
+  children : [{
   		tag : 'p',
   		label : 'myParagraph',
   		innerHTML : 'Hello',
 	    style : {
 	    	backgroundColor : '#000'
-	    }
-    }
-  ]
+	    }  
+  }]
 }, scope);
 
 document.body.appendChild(el);
@@ -89,15 +89,80 @@ myView.helloEl // DOM element
 myView.helloEl.innerHTML // 'hello!'
 
 ```
+## Installation
 
+### With NPM
+
+    $ npm install toDOM
+
+in your script add
+
+```javascript 
+    require("toDOM");
+```
+
+### Client-side
+
+```html 
+    <script src="path/to/toDOM.merged.js">
+```
+or
+
+```html
+    <script src="path/to/toDOM.min.js">
+```
+
+## Basic Usage
+	 You can use sandjs synthax to write both client and server-side code.
+	 see [sandjs](http://github.com/fjs/sandjs) for more informations on sand.js.
+
+```javascript 
+	 var S = sand.require("Seed/Seed", function(r){
+		 var S = r.Seed;
+		 /* code */
+	 });
+```
+	 see [How to require a sandjs module](http://github.com/piercus/sandjs/blob/How-to-require-a-sandjs-module.md) for more ways to require toDOM
 
 ### Also :
 
 * Default tag is 'div' 
-* Is defined in a AMD module if a loader is present, just `require('toDOM')`
+* toDOM is defined with [sandjs](https://github.com/piercus/sandjs) module manager, so you can use it with requirejs if you want to
 
+### Contribute
+## Tests
+
+run tests
+
+    $ npm test
+
+test uses sandcli and run on server-side with vowsjs
+[sandcli](http://github.com/piercus/sandcli) provides a test command
+
+## Documentation
+
+By now documentation is inside the code and uses JSDoc synthax.
+
+## TO DO
+
+*   Make tests on both client-side and server-side
+*   Make a demo
+*   Debug toDjson
+*   Compile documentation form JSDoc
+*   Make performances tests
+   
+## Contribute
+
+Add an issue if you find bugs or please
+
+*   Fork me
+*   Add your tests
+*   Make your contribution
+*   Pass all the tests 
+*   Add a pull request
 
 ### Authors 
-*[Sam Ton That](https://github.com/KspR)
-*[Pierre Cole](https://github.com/piercus)
-*[Cyril Agosta](https://github.com/cagosta)
+*   [Sam Ton That](https://github.com/KspR)
+*   [Pierre Cole](https://github.com/piercus)
+*   [Cyril Agosta](https://github.com/cagosta)
+*   Ilamvazudhi Vijayarangam
--- toDOM.js
+++ toDOM.js
@@ -1,11 +1,14 @@
 sand.define("toDOM/toDOM", ["core/extend"],   
   function(r) {
-  /** @module toDOM/toDOM */     
   
-  /**          
-   *  toDOM is a function that Build an element from a djson object, or a djson-string.
-   *  @param {Object} djson Document-JSON Object
-   *  @param {Object} scope the scope where to put reference defined by the "as" key
+  /** 
+   *  .
+   *  @module toDOM/toDOM
+   *  @exports a function that Build an element from a djson object, or a djson-string        
+   *  
+   *  @param {djson} djson Document-JSON Object 
+   *  @param {Object} scope the scope where to put reference defined by the "as" key                                       
+   *  @see djson
    *  @example Basic example
    *  
    *  var djson = { 
@@ -31,6 +34,26 @@ sand.define("toDOM/toDOM", ["core/extend"],
    *  // if you specify a scope as shown above, scope.foo will be a reference to the element to which you applied it (in this case the "p" element)      
    *
    *  @returns {Object} Returns the HTMLElement 
+   */    
+   
+  /** 
+   *  @namespace djson 
+   *  @example
+   *  { 
+   *    tag : "div", 
+   *    innerHTML : "blabla", 
+   *    style : { 
+   *      backgroundColor : "pink" 
+   *    }, 
+   *    events : { click : function() { return false; } }, 
+   *    attrs : { "class" : "a-class" },
+   *    children : [{ 
+   *        tag : "p", 
+   *        as : "foo", 
+   *      }
+   *    ]
+   *  }
+   *  @desc djson is a format to describe HTML with JSON 
    */
   
   
@@ -87,9 +110,14 @@ sand.define("toDOM/toDOM", ["core/extend"],
   
   var parseString = function(str){
     if(str.match(/\[/)){ 
-     /** 
-      * children are set with [brackets] 
-      * 'div[a, div, yo]' => <div><a></a><div></div><yo></yo></div> 
+     /**
+      * @namespace djson-string 
+      * @desc djson short (string) notation let you use short strings, it's more slow than djson basic notation but it's less verbose
+      * only children, as, tag properties are fully available with json-string notation
+      * you can also use attr.id and attr.class
+      * @see [djson-hybrid] is a mix between djson-string and class djson notation
+      * @example
+      * toDOM('div[a, div, yo]') => &#60;div&#62;&#60;a&#62;&#60;/a&#62;&#60;div&#62;&#60;/div&#62;&#60;yo&#62;&#60;/yo&#62;&#60;/div&#62; 
       */
       return parseChildren(str);
     }
@@ -103,8 +131,10 @@ sand.define("toDOM/toDOM", ["core/extend"],
         className, id, innerHTML, tag,
         res = {},
                 
-     /** 
-      *  as is set with sandjs alias synthax 
+     /**
+      *  @namespace djson-string->as  
+      *  @desc as is set with sandjs alias synthax
+      *  @example 
       *  'a->foo' <=> { tag : 'a', as : "foo"} 
       */
         as = str.split("->")[1],
@@ -119,28 +149,33 @@ sand.define("toDOM/toDOM", ["core/extend"],
     }
     
     var t = str,m;
-    /** 
-     *  className is set with css-selector-like synthax 
-     *  'a.a-class.an-other-class' => <a class="a-class an-other-class"></a> 
+    /**
+     *  @namespace djson-string->className 
+     *  @desc className is set with css-selector-like synthax 
+     *  'a.a-class.an-other-class' => &#60;a class="a-class an-other-class"&#62;&#60;/a&#62; 
      */
     (m = t.match(new RegExp("\\."+strNone, "g"))) && (className = m.map(function(e){return e.slice(1);}).join(" "));
 
-    /** 
-     * id is set with css-selector-like synthax 
-     * 'a#an-id' => <a id="an-id"></a> 
+    /**
+     *  @namespace djson-string->id  
+     *  @desc id is set with css-selector-like synthax 
+     *  'a#an-id' => &#60;a id="an-id"&#62;&#60;/a&#62; 
      */
     (m = t.match(new RegExp("#"+strNone))) && (id =  m[0].slice(1));
     
-    /** 
-     * innerHTML is set with an space, notice 
-     * NB : ' text' does not work, you need to use 'div text' 
-     * 'div yo yo yo ' => <div>yo yo yo </div> 
+    /**
+     *  @namespace djson-string->innerHTML     
+     *  @desc innerHTML is set with an space, notice 
+     *  NB : ' text' does not work, you need to use 'div text'
+     *  @example 
+     *  'div yo yo yo ' => &#60;div&#62;yo yo yo &#60;/div&#62; 
      */   
     (m = t.match(/. .*/)) && (res.innerHTML = m[0].slice(2));
                       
-    /** 
-     * tag is the first characters of the string before any special character (space, #, . ...) 
-     * 'bar   ' => <bar>  </bar>
+    /**
+     *  @namespace djson-string->tag    
+     *  @desc tag is the first characters of the string before any special character (space, #, . ...) 
+     * 'bar   ' => &#60;bar&#62;  &#60;/bar&#62;
      *                                                               
      */        
     (m = t.match(new RegExp("^"+strNone))) && (res.tag = m[0]); 
@@ -165,22 +200,46 @@ sand.define("toDOM/toDOM", ["core/extend"],
     
     if (typeof(obj) === 'string') return toDOM(parseString(obj), scope);
     
-    /** 
-     * @property {string} [div] tag set the tagName
-     *  @example 
-     *  { tag : 'foo' } => <foo></foo>
+
+    /**
+     *  @namespace djson->tag
+     *  @desc  
+     *  NB1 : if you want to create html like '&#60;a&#62;go go &#60;strong&#62;go&#60;strong&#62;!&#60;/a&#62;', you can use the textNode tag
+     *  <br/>NB2 : use short (string) notation inside 'tag', {@link djson-hybrid}
+     *  @property {string} tag set the tagName, default is 'div'
+     *  @example  
+     *  toDOM({ tag : 'foo' }) 
+     *  //=> &#60;foo&#62;&#60;/foo&#62; 
      *
-     *  NB1 : if you want to create html like '<a>go go <strong>go<strong>!</a>', you can use the textNode tag 
-     *  so it will be 
-     *  {tag : "a", children : [{ tag : "textNode", innerHTML : "go go " },{ tag : "strong", innerHTML : "go" },{ tag : "textNode", innerHTML : "!"}]}
-    ¨*
-     *  NB2 : you can use short (string) notation in the tag, it's what we call 'hybrid notation'
-     *  { children : [{ tag : "a.foo.bar" }] } 
-     *  <=>  { children : [{ tag : "a", attr : { className : "foo bar"} }] }
-     *  <=>  { children : ["a.foo.bar"] }
-     *  => <div><a class="foo bar"></a></div>
+     *  toDOM({
+     *      tag : "a", 
+     *      children : [{ 
+     *          tag : "textNode", 
+     *          innerHTML : "go go " 
+     *        },{ 
+     *          tag : "strong", 
+     *          innerHTML : "go" 
+     *        },{ 
+     *          tag : "textNode", 
+     *          innerHTML : "!"
+     *      }]
+     *  }); 
+     *  //=> '&#60;a&#62;go go &#60;strong&#62;go&#60;strong&#62;!&#60;/a&#62;'
+     *   
+
      */    
-     
+     /**
+      *  @namespace djson-hybrid
+      *  @desc Hybrid notation is to use {@link djson-string} short (string) notation,                           
+      *  @example
+      *  // The followings 3 codes are equivalent
+      *  toDOM({ children : [{ tag : "a.foo.bar" }] }); //hybrid-notation
+      *  toDOM({ children : [{ tag : "a", attr : { className : "foo bar"} }] }); // full notation
+      *  toDOM({ children : ["a.foo.bar"] }); // short-notation inside full-notation
+      *
+      *  //=> &#60;div&#62;&#60;a class="foo bar"&#62;&#60;/a&#62;&#60;/div&#62;
+      
+      */     
     if (typeof(obj.tag) === "undefined" || obj.tag === "") {
       if (obj.nodeName) { return (obj); }
       obj.tag = "div";
@@ -209,17 +268,19 @@ sand.define("toDOM/toDOM", ["core/extend"],
       el.className = obj.className;
     }
   
-    /** 
+    /**
+     * @namespace djson->attr 
      * @property {object} attr set attributes 
      * @example
-     * { attr : { foo : 'bar' }} => <div foo="bar"></div> 
+     * { attr : { foo : 'bar' }} => &#60;div foo="bar"&#62;&#60;/div&#62; 
      */
      
     if (obj.attr) 
       for (attr in obj.attr) 
         obj.attr.hasOwnProperty(attr) && obj.attr[attr] && el.setAttribute(attr, obj.attr[attr]);
    
-    /** 
+    /**
+     * @namespace djson->as  
      * @property {string} as set reference key in the scope
      * @example  
      * var el = toDOM({ as : 'foo' }, this);
@@ -229,9 +290,10 @@ sand.define("toDOM/toDOM", ["core/extend"],
     (obj.as)&&(scope[obj.as]=el);
     
     /**
+     * @namespace djson->innerHTML 
      * @property {string|HTMLElement|function} innerHTML set the html inside the tag 
      * @example
-     * { innerHTML : 'my lord <d>Stark </d>' } => '<div>my lord <d>Stark </d></div>'
+     * { innerHTML : 'my lord <d>Stark </d>' } => '&#60;div&#62;my lord &#60;d&#62;Stark &#60;/d&#62;&#60;/div&#62;'
      *  
      * NB : be careful of XSS 
      */
@@ -243,6 +305,7 @@ sand.define("toDOM/toDOM", ["core/extend"],
     }
     
     /** 
+     * @namespace djson->events
      * @property {object} events set the "on..." events (onclick, onover, ...)
      * @property {function} events.click set the "on..." events (onclick, onover, ...)
      * @example 
@@ -253,21 +316,23 @@ sand.define("toDOM/toDOM", ["core/extend"],
      
     if (obj.events) for (var evt in obj.events) (obj.events.hasOwnProperty(evt))&&(el["on"+evt]=obj.events[evt]); 
     
-    /** 
+    /**
+     * @namespace djson->style 
      * @property {object} style key set css style inside the html
      * @example
-     * { style : { backgroundColor : "red" } } => '<div style="background-color:red;"></div>' 
+     * { style : { backgroundColor : "red" } } => '&#60;div style="background-color:red;"&#62;&#60;/div&#62;' 
      *
      * NB : use camelCase keys in style
      */ 
      
     if (obj.style) for (style in obj.style) (obj.style.hasOwnProperty(style))&&(el.style[style]=obj.style[style]);
     
-    /** 
+    /**
+     * @namespace djson->children 
      * @property {array|function} children the children, if function it must return the children array
-     * @property {string|HTMLElement} children[0] 
+     * @property {djson|string|HTMLElement} children[0] 
      * @example
-     * { children : [{ children : [{}] }] } => '<div><div><div></div></div></div>' 
+     * { children : [{ children : [{}] }] } => '&#60;div&#62;&#60;div&#62;&#60;div&#62;&#60;/div&#62;&#60;/div&#62;&#60;/div&#62;' 
      *  NB : if a child is null, he won't be created
      */ 
     
